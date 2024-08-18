17b3b66fe6be88e33ea2a5c9b0ef984b855b1fdc
sherby <109462679+sherbinator@users.noreply.github.com>
2024-08-18 16:42:00 +0200

    add basic tile system

--- entry_ethereal_guardian.c
+++ entry_ethereal_guardian.c
@@ -1,3 +1,36 @@
+const int tile_width = 8;
+
+int world_pos_to_tile_pos(float world_pos) {
+  return roundf(world_pos / (float)tile_width);
+}
+
+float tile_pos_to_world_pos(int tile_pos) {
+  return ((float)tile_pos * (float)tile_width);
+}
+
+Vector2 round_v2_to_tile(Vector2 world_pos) {
+  world_pos.x = tile_pos_to_world_pos(world_pos_to_tile_pos(world_pos.x));
+  world_pos.y = tile_pos_to_world_pos(world_pos_to_tile_pos(world_pos.y));
+  return world_pos;
+}
+
+bool almost_equals(float a, float b, float epsilon) {
+  return fabs(a - b) <= epsilon;
+}
+
+bool animate_f32_to_target(float* value, float target, float delta_t, float rate) {
+  *value += (target - *value) * (1.0 - pow(2.0f, -rate * delta_t));
+  if (almost_equals(*value, target, 0.001f)) {
+    *value = target;
+    return true; // reached
+  }
+  return false;
+}
+
+void animate_v2_to_target(Vector2* value, Vector2 target, float delta_t, float rate) {
+  animate_f32_to_target(&(value->x), target.x, delta_t, rate);
+  animate_f32_to_target(&(value->y), target.y, delta_t, rate);
+}
 
 typedef struct Sprite {
   Gfx_Image* image;
@@ -29,11 +62,11 @@ typedef enum EntityArcheType {
 } EntityArcheType;
 
 typedef struct Entity {
-  bool           is_valid;
+  bool            is_valid;
   EntityArcheType arch;
-  Vector2        pos;
-  bool           render_sprite;
-  SpriteID       sprite_id;
+  Vector2         pos;
+  bool            render_sprite;
+  SpriteID        sprite_id;
 } Entity;
 #define MAX_ENTITY_COUNT 1024
 
@@ -75,6 +108,28 @@ void setup_tree(Entity* en) {
   en->sprite_id = SPRITE_tree0;
 }
 
+Vector2 screen_to_world() {
+  float   mouse_x  = input_frame.mouse_x;
+  float   mouse_y  = input_frame.mouse_y;
+  Matrix4 proj     = draw_frame.projection;
+  Matrix4 view     = draw_frame.view;
+  float   window_w = window.width;
+  float   window_h = window.height;
+
+  // Normalize the mouse coordinates
+  float ndc_x = (mouse_x / (window_w * 0.5f)) - 1.0f;
+  float ndc_y = (mouse_y / (window_h * 0.5f)) - 1.0f;
+
+  // Transform to world coordinates
+  Vector4 world_pos = v4(ndc_x, ndc_y, 0, 1);
+  world_pos         = m4_transform(m4_inverse(proj), world_pos);
+  world_pos         = m4_transform(view, world_pos);
+  // log("%f, %f", world_pos.x, world_pos.y);
+
+  // Return as 2D vector
+  return (Vector2){world_pos.x, world_pos.y};
+}
+
 int entry(int argc, char** argv) {
 
   window.title         = STR("Ethereal Guardian");
@@ -93,6 +148,10 @@ int entry(int argc, char** argv) {
   sprites[SPRITE_tree1]  = (Sprite){.image = load_image_from_disk(STR("./res/sprites/tree1.png"), get_heap_allocator()), .size = v2(7, 12)};
   sprites[SPRITE_rock0]  = (Sprite){.image = load_image_from_disk(STR("./res/sprites/rock0.png"), get_heap_allocator()), .size = v2(7, 4)};
 
+  Gfx_Font* font = load_font_from_disk(STR("C:/windows/fonts/arial.ttf"), get_heap_allocator());
+  assert(font, "Failed loading arial.ttf, %d", GetLastError());
+  const u32 font_height = 48;
+
   Entity* player_en = entity_create();
   setup_player(player_en);
 
@@ -100,16 +159,23 @@ int entry(int argc, char** argv) {
     Entity* en = entity_create();
     setup_rock(en);
     en->pos = v2(get_random_float32_in_range(-200, 200), get_random_float32_in_range(-200, 200));
+    en->pos = round_v2_to_tile(en->pos);
+    en->pos.y -= tile_width * 0.5;
   }
 
   for (int i = 0; i < 10; i++) {
     Entity* en = entity_create();
     setup_tree(en);
     en->pos = v2(get_random_float32_in_range(-200, 200), get_random_float32_in_range(-200, 200));
+    en->pos = round_v2_to_tile(en->pos);
+    en->pos.y -= tile_width * 0.5;
   }
 
   float64 seconds_counter = 0.0;
   s32     frame_count     = 0;
+
+  float   zoom       = 5.3;
+  Vector2 camera_pos = v2(0, 0);
 
   float64 last_time = os_get_current_time_in_seconds();
   while (!window.should_close) {
@@ -117,16 +183,51 @@ int entry(int argc, char** argv) {
 
     draw_frame.projection = m4_make_orthographic_projection(window.width * -0.5, window.width * 0.5, window.height * -0.5, window.height * 0.5, -1, 10);
 
-    float zoom      = 5.3;
-    draw_frame.view = m4_make_scale(v3(1.0 / zoom, 1.0 / zoom, 1.0));
-
     float64 now     = os_get_current_time_in_seconds();
     float64 delta_t = now - last_time;
     last_time       = now;
-
     os_update();
 
-    // render
+    {
+      Vector2 target_pos = player_en->pos;
+      animate_v2_to_target(&camera_pos, target_pos, delta_t, 30.0f);
+
+      draw_frame.view = m4_make_scale(v3(1.0, 1.0, 1.0));
+      draw_frame.view = m4_mul(draw_frame.view, m4_make_translation(v3(camera_pos.x, camera_pos.y, 0)));
+      draw_frame.view = m4_mul(draw_frame.view, m4_make_scale(v3(1.0 / zoom, 1.0 / zoom, 1.0)));
+    }
+
+    Vector2 mouse_pos    = screen_to_world();
+    int     mouse_tile_x = world_pos_to_tile_pos(mouse_pos.x);
+    int     mouse_tile_y = world_pos_to_tile_pos(mouse_pos.y);
+
+    {
+      Vector2 pos = screen_to_world();
+      // log("%f, %f", pos.x, pos.y);
+      draw_text(font, tprint("%f %f", pos.x, pos.y), font_height, pos, v2(0.1, 0.1), COLOR_RED);
+    }
+
+    // :tile rendering
+    {
+      int player_tile_x = world_pos_to_tile_pos(player_en->pos.x);
+      int player_tile_y = world_pos_to_tile_pos(player_en->pos.y);
+      int tile_radius_x = 40;
+      int tile_radius_y = 30;
+      for (int x = player_tile_x - tile_radius_x; x < player_tile_x + tile_radius_x; x++) {
+        for (int y = player_tile_y - tile_radius_y; y < player_tile_y + tile_radius_y; y++) {
+          if ((x + (y % 2 == 0)) % 2 == 0) {
+            Vector4 col   = v4(0.1, 0.1, 0.1, 0.1);
+            float   x_pos = x * tile_width;
+            float   y_pos = y * tile_width;
+            draw_rect(v2(x_pos + tile_width * -0.5, y_pos + tile_width * -0.5), v2(tile_width, tile_width), col);
+          }
+        }
+      }
+
+      draw_rect(v2(tile_pos_to_world_pos(mouse_tile_x) + tile_width * -0.5, tile_pos_to_world_pos(mouse_tile_y) + tile_width * -0.5), v2(tile_width, tile_width), v4(0.5, 0.5, 0.5, 0.5));
+    }
+
+    // :render
     for (int i = 0; i < MAX_ENTITY_COUNT; i++) {
       Entity* en = &world->entities[i];
       if (en->is_valid) {
@@ -137,6 +238,9 @@ int entry(int argc, char** argv) {
             xform          = m4_translate(xform, v3(en->pos.x, en->pos.y, 0));
             xform          = m4_translate(xform, v3(sprite->size.x * -0.5, 0.0, 0));
             draw_image_xform(sprite->image, xform, sprite->size, COLOR_WHITE);
+
+            draw_text(font, tprint("%f %f", en->pos.x, en->pos.y), font_height, en->pos, v2(0.1, 0.1), COLOR_WHITE);
+
             break;
           }
         }
